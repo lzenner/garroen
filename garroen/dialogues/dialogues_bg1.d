4ef4d6d0c5a3f5f6f25beec61254e97961369d94
lzenner <lzenner@live.com>
2020-09-30 14:56:53 -1000

    Pretty much redid everything

--- garroen/dialogues/dialogues_bg1.d
+++ garroen/dialogues/dialogues_bg1.d
@@ -1,28 +1,52 @@
 //////////////////////////////////////////////
 // Post dialogues for BG1                   //
-// Makes sure they both go to Fieldpost Inn //
+// Makes sure they both go to Feldepost Inn //
 //////////////////////////////////////////////
 
-// Imoen seated at 700.545 facing NE
-// Will need to get rid of actor at 
-EXTEND_BOTTOM %GARRICK_POST% 0
-	IF ~Global("KickedOut","LOCALS",0)~ GOTO GARRICK_TO_FIELDPOST
+EXTEND_BOTTOM %GARRICK_POST% 0 1
+	IF ~!AreaCheck("%Beregost_FeldepostsInn_L1%")~ GOTO GARRICK_TO_FELDEPOST
+	IF ~AreaCheck("%Beregost_FeldepostsInn_L1%")~ GOTO GARRICK_AT_FELDEPOST
 END
 
+ALTER_TRANS %GARRICK_POST%
+	BEGIN 3 END
+	BEGIN 0 END
+	BEGIN
+		"ACTION" ~SetGlobal("KickedOut","LOCALS",0) SetGlobal("#L_GarrickFeldepost","GLOBAL",0) JoinParty()~
+	END
+
 APPEND %GARRICK_POST%
-	IF ~~ BEGIN GARRICK_TO_FIELDPOST
-		SAY @2000 /* ~I'll be at the Fieldpost Inn in Beregost if you need me in the future.~ */
-		IF ~~ DO ~SetGlobalTimer("#L_GIParkedDay","GLOBAL",ONE_DAY) SetGlobal("#L_GarrickFieldpost","GLOBAL",1) SetGlobal("KickedOut","LOCALS",1) ChangeAIScript("",DEFAULT) EscapeAreaMove("%Beregost_FeldepostsInn_L1%",900,510,SW)~ EXIT
+	IF ~~ BEGIN GARRICK_TO_FELDEPOST
+		SAY @2000 /* ~I'll be at the Feldepost Inn in Beregost if you need me in the future.~ */
+		IF ~~ DO ~SetGlobal("#L_GarrickFeldepost","GLOBAL",1) SetGlobal("KickedOut","LOCALS",1) ChangeAIScript("",DEFAULT) EscapeAreaMove("%Beregost_FeldepostsInn_L1%",900,510,SW)~ EXIT
+		IF ~Global("T1M2LetterTimer","GLOBAL",0)~ DO ~SetGlobalTimer("T1M2LetterTimer","GLOBAL",TWO_DAYS) SetGlobal("#L_GarrickFeldepost","GLOBAL",1) SetGlobal("KickedOut","LOCALS",1) ChangeAIScript("",DEFAULT) EscapeAreaMove("%Beregost_FeldepostsInn_L1%",900,510,SW)~ EXIT
+	END
+
+
+	IF ~~ BEGIN GARRICK_AT_FELDEPOST
+		SAY @2001 /* ~And this is as good a place as any to perform.~ */
+		IF ~~ DO ~SetGlobal("#L_GarrickFeldepost","GLOBAL",1) SetGlobal("KickedOut","LOCALS",1) ChangeAIScript("",DEFAULT) MoveToPoint([900.510]) Face(SW)~ EXIT
+		IF ~Global("T1M2LetterTimer","GLOBAL",0)~ DO ~SetGlobalTimer("T1M2LetterTimer","GLOBAL",TWO_DAYS) SetGlobal("#L_GarrickFeldepost","GLOBAL",1) SetGlobal("KickedOut","LOCALS",1) ChangeAIScript("",DEFAULT) MoveToPoint([900.510]) Face(SW)~ EXIT
+		IF ~Global("KickedOut","LOCALS",1)~ EXIT
 	END
 END
 
 EXTEND_BOTTOM %IMOEN_POST% 0 1
-	IF ~Global("KickedOut","LOCALS",0)~ GOTO IMOEN_TO_FIELDPOST
+	IF ~!AreaCheck("%Beregost_FeldepostsInn_L1%")~ GOTO IMOEN_TO_FELDEPOST
+	IF ~AreaCheck("%Beregost_FeldepostsInn_L1%")~ GOTO IMOEN_AT_FELDEPOST
 END
 
 APPEND %IMOEN_POST%
-	IF ~~ BEGIN IMOEN_TO_FIELDPOST
-		SAY @2000 /* ~I'll be at the Fieldpost Inn in Beregost if you need me in the future.~ */
-		IF ~~ DO ~SetGlobalTimer("#L_GIParkedDay","GLOBAL",ONE_DAY) SetGlobal("#L_ImoenFieldpost","GLOBAL",1) SetGlobal("KickedOut","LOCALS",1) ChangeAIScript("",DEFAULT) EscapeAreaMove("%Beregost_FeldepostsInn_L1%",780,580,NE)~ EXIT
+	IF ~~ BEGIN IMOEN_TO_FELDEPOST
+		SAY @2000 /* ~I'll be at the Feldepost Inn in Beregost if you need me in the future.~ */
+		IF ~~ DO ~SetGlobal("#L_ImoenFeldepost","GLOBAL",1) SetGlobal("KickedOut","LOCALS",1) ChangeAIScript("",DEFAULT) EscapeAreaMove("%Beregost_FeldepostsInn_L1%",780,580,NE)~ EXIT
+		IF ~Global("T1M2LetterTimer","GLOBAL",0)~ DO ~SetGlobalTimer("T1M2LetterTimer","GLOBAL",TWO_DAYS) SetGlobal("#L_ImoenFeldepost","GLOBAL",1) SetGlobal("KickedOut","LOCALS",1) ChangeAIScript("",DEFAULT) EscapeAreaMove("%Beregost_FeldepostsInn_L1%",780,580,NE)~ EXIT
+	END
+
+	IF ~~ BEGIN IMOEN_AT_FELDEPOST
+		SAY @2002 /* ~I'll be right here if you need me in the future.~ */
+		IF ~~ DO ~SetGlobal("#L_ImoenFeldepost","GLOBAL",1) SetGlobal("KickedOut","LOCALS",1) ChangeAIScript("",DEFAULT) MoveToPoint([900.510]) Face(NE)~ EXIT
+		IF ~Global("T1M2LetterTimer","GLOBAL",0)~ DO ~SetGlobalTimer("T1M2LetterTimer","GLOBAL",TWO_DAYS) SetGlobal("#L_ImoenFeldepost","GLOBAL",1) SetGlobal("KickedOut","LOCALS",1) ChangeAIScript("",DEFAULT) MoveToPoint([780.580]) Face(NE)~ EXIT
+		IF ~Global("KickedOut","LOCALS",1)~ EXIT
 	END
 END
--- garroen/readme.garroen.english.txt
+++ garroen/readme.garroen.english.txt
@@ -18,7 +18,7 @@ DESCRIPTION
 BG1EE Content
 ------------------------------------------------------------------------
 Requires BG1EE or EET
-The romance will begin if Garrick and Imoen are both in the group or are 'parked' in the same location.  If the Garrick's Infatuation mod is installed and both he and Imoen are in the group, the romance will begin after he declares you are his best friend.  Whatever remains of the infatuation will be skipped once their romance begins.
+The romance will begin if Garrick and Imoen are both in the group or are 'parked' in Feldepost's Inn.  If the Garrick's Infatuation mod is installed and both he and Imoen are in the group, the romance will begin after he declares you are his best friend and the rest of the infatuation will be skipped.  The infatuation will also end if they are parked together in Feldepost's Inn for more than 5 days. If you force them to part company, the romance will pause until they're either together again or SoD starts.
 
 ------------------------------------------------------------------------
 SoD Content
