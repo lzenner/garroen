330c2162798a0bc9c073808b30eecf4522f0b11a
lzenner <lzenner@live.com>
2020-10-03 13:31:45 -1000

    Changes needed to move Garrick to Three Old Kegs when Imoen is in training (Transitions compatibility)

--- garroen/dialogues/dialogues_bg1.d
+++ garroen/dialogues/dialogues_bg1.d
@@ -8,35 +8,60 @@ ALTER_TRANS GARRIC
 	BEGIN 3 6 12 END
 	BEGIN 0 END
 	BEGIN
+		"TRIGGER" ~Global("#L_ImoenInPalace","GLOBAL",0)~
 		"ACTION" ~~
 		"EPILOGUE" ~GOTO GARRIC_TO_FELDEPOST~
 	END
+EXTEND_TOP GARRIC 3 #0
+	IF ~Global("#L_ImoenInPalace","GLOBAL",1)~ THEN REPLY #200552 GOTO GARRIC_TO_TOK
+END
+EXTEND_TOP GARRIC 6 #0
+	IF ~Global("#L_ImoenInPalace","GLOBAL",1)~ THEN REPLY #200556 GOTO GARRIC_TO_TOK
+END
+EXTEND_TOP GARRIC 12 #0
+	IF ~Global("#L_ImoenInPalace","GLOBAL",1)~ THEN REPLY #214735 GOTO GARRIC_TO_TOK
+END
 
 ALTER_TRANS GARRIC 
 	BEGIN 7 END
 	BEGIN 1 END
 	BEGIN
+		"TRIGGER" ~Global("#L_ImoenInPalace","GLOBAL",0)~
 		"EPILOGUE" ~GOTO GARRIC_TO_FELDEPOST~
 	END
+EXTEND_TOP GARRIC 7 #1
+	IF ~Global("#L_ImoenInPalace","GLOBAL",1)~ THEN REPLY #200557 GOTO GARRIC_TO_TOK
+END
 
 ALTER_TRANS GARRIC 
 	BEGIN 13 END
 	BEGIN 0 END
 	BEGIN
+		"TRIGGER" ~Global("#L_ImoenInPalace","GLOBAL",0)~
 		"ACTION" ~SetDialogue("GARRIJ") LeaveParty()~
 		"EPILOGUE" ~GOTO GARRIC_TO_FELDEPOST~
 	END
+EXTEND_TOP GARRIC 13 #0
+	IF ~Global("#L_ImoenInPalace","GLOBAL",1)~ THEN REPLY #214736 DO ~SetDialogue("GARRIJ") LeaveParty()~ GOTO GARRIC_TO_TOK
+END
 
 APPEND GARRIC
 	IF ~~ BEGIN GARRIC_TO_FELDEPOST
 		SAY @2000 /* ~I'll be at the Feldepost Inn in Beregost if you need me in the future.~ */
 		IF ~~ DO ~SetGlobal("#L_GarrickFeldepost","GLOBAL",1) SetGlobal("KickedOut","LOCALS",1) ChangeAIScript("",DEFAULT) EscapeAreaMove("%Beregost_FeldepostsInn_L1%",900,510,SW)~ EXIT
 	END
+
+	IF ~~ BEGIN GARRIC_TO_TOK
+		SAY	@2003 // ~I'll be at Three Old Keg's in North Baldur's Gate if you need me in the future.~
+		IF ~~ DO ~SetGlobal("#L_GarrickTOK","GLOBAL",1) SetGlobal("KickedOut","LOCALS",1) ChangeAIScript("",DEFAULT) EscapeAreaMove("%NBaldursGate_ThreeOldKegs_L1%",350,140,SE)~ EXIT
+	END
 END
 
 EXTEND_BOTTOM %GARRICK_POST% 0 1
-	IF ~!AreaCheck("%Beregost_FeldepostsInn_L1%")~ GOTO GARRICK_TO_FELDEPOST
-	IF ~AreaCheck("%Beregost_FeldepostsInn_L1%")~ GOTO GARRICK_AT_FELDEPOST
+	IF ~!AreaCheck("%Beregost_FeldepostsInn_L1%") Global("#L_ImoenInPalace","GLOBAL",0)~ GOTO GARRICK_TO_FELDEPOST
+	IF ~AreaCheck("%Beregost_FeldepostsInn_L1%") Global("#L_ImoenInPalace","GLOBAL",0)~ GOTO GARRICK_AT_FELDEPOST
+	IF ~!AreaCheck("%NBaldursGate_ThreeOldKegs_L1%") Global("#L_ImoenInPalace","GLOBAL",1)~ GOTO GARRICK_TO_TOK
+	IF ~AreaCheck("%NBaldursGate_ThreeOldKegs_L1%") Global("#L_ImoenInPalace","GLOBAL",1)~ GOTO GARRICK_AT_TOK
 END
 
 // Shake off all the mods trying to send garrick to other areas
@@ -65,6 +90,17 @@ APPEND %GARRICK_POST%
 	IF ~~ BEGIN GARRICK_AT_FELDEPOST
 		SAY @2001 /* ~And this is as good a place as any to perform.~ */
 		IF ~~ DO ~SetGlobal("#L_GarrickFeldepost","GLOBAL",1) SetGlobal("KickedOut","LOCALS",1) ChangeAIScript("",DEFAULT) MoveToPoint([900.510]) Face(SW)~ EXIT
+		IF ~Global("KickedOut","LOCALS",1)~ EXIT
+	END
+
+	IF ~~ BEGIN GARRICK_TO_TOK
+		SAY	@2003 // ~I'll be at Three Old Keg's in North Baldur's Gate if you need me in the future.~
+		IF ~~ DO ~SetGlobal("#L_GarrickTOK","GLOBAL",1) SetGlobal("KickedOut","LOCALS",1) ChangeAIScript("",DEFAULT) EscapeAreaMove("%NBaldursGate_ThreeOldKegs_L1%",350,140,SE)~ EXIT
+	END
+
+	IF ~~ BEGIN GARRICK_AT_TOK
+		SAY @2001 /* ~And this is as good a place as any to perform.~ */
+		IF ~~ DO ~SetGlobal("#L_GarrickTOK","GLOBAL",1) SetGlobal("KickedOut","LOCALS",1) ChangeAIScript("",DEFAULT) MoveToPoint([350.140]) Face(SE)~ EXIT
 		IF ~Global("KickedOut","LOCALS",1)~ EXIT
 	END
 END
